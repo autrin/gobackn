bfe207f365f6fbacf3efc2d03d1b3bafd4e51d01
autrin <autrinhakimi@gmail.com>
2024-11-29 10:40:21 -0600

    added some comments for explanation

--- ccitt16.h
+++ ccitt16.h
@@ -6,9 +6,9 @@ extern "C"
 {
 #endif
 
-#define GENERATE_CRC 1
-#define CHECK_CRC 2
-
+#define GENERATE_CRC 1 // Returns the checksum of cData[] with length iLen as a short int
+#define CHECK_CRC 2 // Uses the last two bytes of cData[] as CRC check bits to check cData[]; returns either 0 or 1:
+                    // CRC_CHECK_SUCCESSFUL or CRC_CHECK_FAILURE
 #define CRC_CHECK_SUCCESSFUL 0
 #define CRC_CHECK_FAILURE 1
 
@@ -16,7 +16,7 @@ extern "C"
 #define FEEDBACK(a) (a = (a ^ CCITT_POLYNOMS))
 
   short int calculate_CCITT16(unsigned char cData[], unsigned int iLen, unsigned int iAction);
-
+  // iAction is defined as either GENERATE_CRC or CHECK_CRC
 #ifdef __cplusplus
 }
 #endif
