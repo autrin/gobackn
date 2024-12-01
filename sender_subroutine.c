6ec39ebc398940c5b8bddebb1731250e6d5d93f3
autrin <autrinhakimi@gmail.com>
2024-12-01 13:23:03 -0600

    send the packet

--- sender_subroutine.c
+++ sender_subroutine.c
@@ -44,19 +44,27 @@ void primary(int sockfd, double ber) {
             };
             build_packet(packet, PKT_TYPE_DATA, send_msg, next_seq_num);
 
-            // Apply CRC to the packet (generate CRC)
+            // Generate CRC
             short int crc = calculate_CCITT16((unsigned char *)packet, PKT_SIZE - 2, GENERATE_CRC);
             // PKT-2 Excludes the last two bytes of the packet (reserved for CRC) when generating the CRC.
             
             // Set CRC into the packet
             packet->crc_sum[0] = (crc >> 8) & 0xFF; // High byte
             packet->crc_sum[1] = crc & 0xFF;        // Low byte
-            // introduce error based on the bit rate error
+
+            // Apply BER to the packet
             introduce_bit_error((char *)packet, PKT_SIZE, ber);
             // Notice that if the data is delivered corrupt, it needs to be redelivered
-            printf("Built packet and applied ber\n");
-            print_packet((packet_t *)&send_msg);
-            pack_num++;
+            // printf("Built packet and applied ber\n");
+            // print_packet((packet_t *)&send_msg);
+            // pack_num++;
+            
+            // Send the packet
+            if (send(sockfd, (char *)packet, sizeof(packet_t), 0) < 0) {
+                perror("Send failed");
+                free(packet);
+                return;
+            }
         }
     }
     // Implement Go-Back-N ARQ protocol:
